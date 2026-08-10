ace7adf636ee435f72d8083b00466653243977ae
Jayden Lim <jaydenlim43@gmail.com>
2026-08-09 22:45:40 -0400

    Implement system prompt

--- models/eval.py
+++ models/eval.py
@@ -3,58 +3,78 @@ from transformers import AutoProcessor, AutoModelForMultimodalLM
 
 MODEL = "Qwen/Qwen3.5-9B"
 
+SYSTEM_PROMPT = """
+You are Mavis, a voice-assistant that is experienced in: 
+1. Stock market analysis
+2. Teaching mandarin to native English speakers
+3. Health and fitness advice for gym-goers 
+
+You must respond only respond with your final answer and hide your thinking process from the user. Restrict your response to 4 sentences max. 
+
+"""
+
 print("MPS available:", torch.backends.mps.is_available())
 
 processor = AutoProcessor.from_pretrained(MODEL)
 
 print("Loading model...")
 
-model = AutoModelForMultimodalLM.from_pretrained(
-    "Qwen/Qwen3.5-9B",
-    dtype=torch.float16,
-).to("mps")
 
-print("Model loaded")
-print(next(model.parameters()).device)
+if __name__ == "__main__":
+    model = AutoModelForMultimodalLM.from_pretrained(
+        "Qwen/Qwen3.5-4B",
+        dtype=torch.float16,
+    ).to("mps")
 
-user_msg = input("Type something: ")
+    print("Model loaded")
+    print(next(model.parameters()).device)
+    user_msg = input("Type something: ")
 
-messages = [
-    {
-        "role": "user",
-        "content": [
+    while user_msg != "terminate":
+        messages = [
             {
-                "type": "text",
-                "text": f"{user_msg}"
+                "role": "system",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": SYSTEM_PROMPT
+                    }
+                ]
+            },
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": user_msg
+                    }
+                ]
             }
         ]
-    }
-]
 
-print("Processing image...")
 
-inputs = processor.apply_chat_template(
-    messages,
-    add_generation_prompt=True,
-    tokenize=True,
-    return_dict=True,
-    return_tensors="pt",
-)
+        inputs = processor.apply_chat_template(
+            messages,
+            add_generation_prompt=True,
+            tokenize=True,
+            return_dict=True,
+            return_tensors="pt",
+            enable_thinking=False,
+        )
 
-inputs = inputs.to(model.device)
+        inputs = inputs.to(model.device)
 
-print("Starting generation...")
+        print("Starting generation...")
 
-outputs = model.generate(
-    **inputs,
-    max_new_tokens=10,
-)
+        outputs = model.generate(
+            **inputs,
+            max_new_tokens=40,
+        )
 
-print("Generation complete!")
+        answer = processor.decode(
+            outputs[0][inputs["input_ids"].shape[-1]:],
+            skip_special_tokens=True,
+        )
 
-answer = processor.decode(
-    outputs[0][inputs["input_ids"].shape[-1]:],
-    skip_special_tokens=True,
-)
-
-print("Answer:", answer)
+        print("Answer:", answer)
+        user_msg = input("Type something: ")
